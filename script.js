a0925fe8662530dbd6812b9836af33df525fe2a8
Baby Vitalik <166022574+eth6900@users.noreply.github.com>
2024-10-08 23:29:26 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-$( document ).ready(function() {
-    $( ".tradingview-widget-container iframe" ).clone().appendTo( ".tradingview-widget-container" );
-  });
-  
-  
+// $( document ).ready(function() {
+//     $( ".tradingview-widget-container iframe" ).clone().appendTo( ".tradingview-widget-container" );
+//   });
+
+// Create a new Audio object
+const audio = new Audio('audio.mp3'); // Replace with the path to your audio file
+// Set the audio to loop
+audio.loop = true;
+
+// Function to play the audio
+function playAudioLoop() {
+  audio.play();
+}
+
+function enterWebsite() {
+  document.getElementById('entryModal').style.display = 'none'; // Hide the modal
+  //document.getElementById('mainContent').style.display = 'block'; // Show the main content
+  playAudioLoop();
+}
